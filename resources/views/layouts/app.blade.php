2c402da1cb6424b6e4f2f04640417665372e2778
Matt Leung <8941112+mdleung@users.noreply.github.com>
2025-06-26 18:38:38 -0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,7 +129,16 @@
     <meta name="format-detection" content="address=no">
     <meta name="format-detection" content="email=no">
 
+        <!-- Skip navigation for screen readers -->
+    <meta name="skip-link" content="#main-content">
+    
+    <!-- Screen reader optimization -->
+    <meta name="screen-reader-optimized" content="true">
 
+    <link rel="icon" type="image/x-icon" href="https://givingtalents.org/public/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://givingtalents.org/public//favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://givingtalents.org/public//favicon-16x16.png">
+    
         <script type="application/ld+json">
     {
       "@context": "https://schema.org",
